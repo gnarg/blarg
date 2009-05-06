cfc39f31130e00dfda90edbb8d35b112c86cbfa2
gnarg <guymon@slackworks.com>
2009-05-06 15:38:57 -0700

    show individual posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,13 @@ class Post < Sofa
       }
     JS
   end
+  
+  def self.get_by_slug(slug)
+    results = self.temp_view <<-JS
+      if (doc.document_type == 'Post' && doc.slug == '#{slug}') {
+        emit(null, doc) 
+      }
+    JS
+    results.first
+  end
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   test "#comments returns the associated comments" do
+    clean_couch
     post = Post.create(:title => 'new post')
     comment1 = Comment.create(:post_id => post.id)
     comment2 = Comment.create(:post_id => post.id)
@@ -10,5 +11,13 @@ class PostTest < ActiveSupport::TestCase
     
     assert post_comments.include?(comment1)
     assert post_comments.include?(comment2)    
+  end
+  
+  test ".get_by_slug returns the post with given slug" do
+    clean_couch
+    saved_post = Post.create(:slug => 'test-slug')
+    found_post = Post.get_by_slug('test-slug')
+    
+    assert_equal saved_post, found_post
   end
 end
