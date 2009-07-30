3b4c0af47dca67c5b93fbf3780b3a01b618831bb
Nic Benders <nic.benders@gmail.com>
2009-07-29 18:19:31 -0700

    Add find_all and find_all_unapproved to the Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,20 @@
 class Comment < Sofa::Record  
   validates_present :body
+
+  def self.find_all
+    self.temp_view <<-JS
+      if (doc.document_type == 'Comment') {
+        emit(doc.created_at, doc)
+      }
+    JS
+  end
+
+  def self.find_all_unapproved
+    self.temp_view <<-JS
+      if (doc.document_type == 'Comment' && doc.approved != true) {
+        emit(doc.created_at, doc)
+      }
+    JS
+  end
+
 end
